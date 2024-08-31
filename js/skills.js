f82adcd9575398e6fbfd96fc31bf60614cf3ff7f
Aida Halitaj <halitajaida@gmail.com>
2024-08-31 01:08:25 +0100

    updating skills

--- js/skills.js
+++ js/skills.js
@@ -1,23 +1,43 @@
-const skills = [
-    "Python", "HTML5", "CSS3",
-    "Git", "SQL", "MongoDB", "DynamoDB", "MySQL", "Docker", "AWS",
-    "Pytorch", "Tensorflow", "Keras", "OpenCV", "Pandas", "Numpy",
-     "Scikit-learn", "SciPy", "Matplotlib", "Seaborn", "Plotly", 
-     "Bokeh"]
+const skillCategories = {
+    "Programming Languages": ["Python", "HTML5", "CSS3", "SQL"],
+    "Databases": ["MongoDB (NoSQL Database)", "DynamoDB (NoSQL Database)", "MySQL (Relational Database)"],
+    "Machine Learning & AI": ["Pytorch (Deep Learning Framework)", "Tensorflow (Deep Learning Framework)", "Keras (Neural Network Library)", "Scikit-learn (Machine Learning Library)"],
+    "Data Science & Analytics": ["Pandas (Data Manipulation)", "Numpy (Numerical Computation)", "SciPy (Scientific Computing)", "Matplotlib (Data Visualization)", "Seaborn (Statistical Data Visualization)", "Plotly (Interactive Data Visualization)", "Bokeh (Interactive Data Visualization)"],
+    "Web Development": ["HTML5", "CSS3"],
+    "Version Control & DevOps": ["Git (Version Control)", "Docker (Containerization)", "AWS (Cloud Services)"]
+};
 
-function createSkillsList(skills) {
-    return `
-        <div class="col-md-12">
-            <ul class="skills-list">
+function createSkillsAccordion(categories) {
+    return Object.entries(categories).map(([category, skills]) => `
+        <li>
+            <div class="link">
+                <p style="margin-bottom: 0px">${category}</p>
+                <i class="fa fa-chevron-down"></i>
+            </div>
+            <ul class="submenu">
                 ${skills.map(skill => `<li>${skill}</li>`).join('')}
             </ul>
-        </div>
-    `;
+        </li>
+    `).join('');
 }
 
 function populateSkills() {
-    const skillsContainer = document.getElementById('skills-container');
-    skillsContainer.innerHTML = createSkillsList(skills);
+    const skillsAccordion = document.getElementById('skills-accordion');
+    skillsAccordion.innerHTML = createSkillsAccordion(skillCategories);
+
+    // Add click event listeners to toggle skill lists
+    const toggles = skillsAccordion.querySelectorAll('.link');
+    toggles.forEach(toggle => {
+        toggle.addEventListener('click', function() {
+            this.classList.toggle('open');
+            const submenu = this.nextElementSibling;
+            if (submenu.style.display === "block") {
+                submenu.style.display = "none";
+            } else {
+                submenu.style.display = "block";
+            }
+        });
+    });
 }
 
 document.addEventListener('DOMContentLoaded', populateSkills);
